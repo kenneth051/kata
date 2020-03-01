4b0ba139e535e10de3309e15eece948ba55b1008
kenneth051 <kjdumba@gmail.com>
2020-03-01 21:22:59 +0200

    bank account initial tests

---
+++ bank/account.py
@@ -0,0 +1,36 @@
+from unittest import TestCase, skip
+from unittest.mock import Mock, patch, call
+import io
+import sys
+from datetime import date
+
+class WithdrawMoney:
+    def __init__(self, amount, date):
+        pass
+
+class AddMoney:
+    def __init__(self, amount,date,boy="hello"):
+        print(amount, date, boy)
+        pass
+
+class Statement:
+    def __init__(self):
+        pass
+
+class Account:
+    def __init__(self,history, output=sys.stdout):
+        self.history = history
+        self.output = output
+    
+    def deposit(self, money):
+        transaction = AddMoney(money, date.today())
+        self.history.add(transaction)
+
+    def withdraw(self, amount):
+        transaction = WithdrawMoney(amount, date.today())
+        self.history.deduct(transaction)
+    
+    def printStatement(self):
+        statement=Statement(self.history)
+        self.output.write(statement)
+        
