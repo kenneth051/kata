7e7e5e161c3c31b3a250e5d147b42979c714cf37
kenneth051 <kjdumba@gmail.com>
2020-12-25 23:28:30 +0300

    format prime factors

--- prime_factors/test_prime_factors.py
+++ prime_factors/test_prime_factors.py
@@ -1,32 +1,34 @@
 import unittest
 
+
 def prime_factors(number):
     """method to get prime factors of a number
-    
+
     Arguments:
         number {int} -- input number to get its prime factors.
-    
+
     Returns:
         List -- [A list of prime factors.
     """
-    factors=[]
-    
-    #checks if the input is less than 2 then returns an empty list.
+    factors = []
+
+    # checks if the input is less than 2 then returns an empty list.
     if number < 2:
         return []
 
-    #checks if the input is equal to 2 then returns 1 in a list.
+    # checks if the input is equal to 2 then returns 1 in a list.
     if number == 2:
         return [1]
 
-    for factor in range(2,number+1):
-        #condition to check whether the input number when divided by the number in iteration gives a remainder 0
+    for factor in range(2, number + 1):
+        # condition to check whether the input number when divided by the number in iteration gives a remainder 0
         while number % factor == 0:
             factors.append(factor)
-            number=number/factor
+            number = number / factor
 
     return factors
-    
+
+
 class TestPrimeFactors(unittest.TestCase):
     """Prime factors test class"""
 
@@ -43,19 +45,20 @@ class TestPrimeFactors(unittest.TestCase):
         self.assertEquals(prime_factors(3), [3])
 
     def test_4(self):
-        self.assertEquals(prime_factors(4), [2,2])
+        self.assertEquals(prime_factors(4), [2, 2])
 
     def test_5(self):
         self.assertEquals(prime_factors(5), [5])
 
     def test_8(self):
-            self.assertEquals(prime_factors(8), [2,2,2])
- 
+        self.assertEquals(prime_factors(8), [2, 2, 2])
+
     def test_9(self):
-        self.assertEquals(prime_factors(9), [3,3])
+        self.assertEquals(prime_factors(9), [3, 3])
 
     def test_25(self):
-        self.assertEquals(prime_factors(25), [5,5])
+        self.assertEquals(prime_factors(25), [5, 5])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
