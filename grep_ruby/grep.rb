9015c656fe59a17b6ec706373b1df030e5ec08b3
kenneth051 <kjdumba@gmail.com>
2020-12-25 23:27:35 +0300

    format grep kata

--- grep_ruby/grep.rb
+++ grep_ruby/grep.rb
@@ -1,5 +1,5 @@
 # Search a file for lines matching a regular expression pattern. Return the line number and contents of each matching line.
-# Your task is to implement the grep function, which should read the contents of the specified files, find the lines that match the specified pattern 
+# Your task is to implement the grep function, which should read the contents of the specified files, find the lines that match the specified pattern
 # and then output those lines as a single string. Note that the lines should be output in the order in which they were found,
 # with the first matching line in the first file being output first.
 # As an example, suppose there is a file named "input.txt" with the following contents:
@@ -12,7 +12,7 @@
 #     hello again
 
 # Flags
-# As said earlier, the grep command should also support the following flags:    
+# As said earlier, the grep command should also support the following flags:
 #     -n Print the line numbers of each matching line.
 #     -l Print only the names of files that contain at least one matching line.
 #     -i Match line using a case-insensitive comparison.
@@ -24,69 +24,71 @@
 #refactoring big functions
 
 class Grep
-    def initialize(keyword,file,flags)
-        @keyword =keyword
-        @file = file
-        @matched_lines =[]
-        @matched_files =[]
-        @flags =flags
-    end
+  def initialize(keyword, file, flags)
+    @keyword = keyword
+    @file = file
+    @matched_lines = []
+    @matched_files = []
+    @flags = flags
+  end
 
-    def matches
-        return @matched_files.uniq if match_filenames?
-        @matched_lines
-    end
-    def case_sensitive?
-        @flags.include?("-i")
-    end
-    def matches_entire_line?
-        @flags.include?("-x")
-    end
-    def match_filenames?
-        @flags.include?("-l")
-    end
+  def matches
+    return @matched_files.uniq if match_filenames?
+    @matched_lines
+  end
 
-    def pattern
-        Regexp.new @keyword, (Regexp::IGNORECASE if case_sensitive?)
-    end
+  def case_sensitive?
+    @flags.include?("-i")
+  end
 
-    def with_line_number_flag(line,line_index)
-        if @flags.include?("-n")
-            line ="line#{line_index+1}: #{line}"
-        end
-        line
-    end
+  def matches_entire_line?
+    @flags.include?("-x")
+  end
 
-    def save_matches(line, file)
-        @matched_lines << line
-        @matched_files << file
-    end
+  def match_filenames?
+    @flags.include?("-l")
+  end
 
-    def matches_line_only(line, file)
-        if !matches_entire_line? && line.match?(pattern)
-            save_matches(line, file)
-        end
-    end
+  def pattern
+    Regexp.new @keyword, (Regexp::IGNORECASE if case_sensitive?)
+  end
 
-    def matches_entire_line_only(line, file)
-        if matches_entire_line? &&(line.downcase == @keyword.downcase)&& line.match?(pattern)
-            save_matches(line, file)
-        end
+  def with_line_number_flag(line, line_index)
+    if @flags.include?("-n")
+      line = "line#{line_index + 1}: #{line}"
     end
+    line
+  end
 
-    def search_matches
-        @file.map do|file|
-            f = File.open(file,"r")
-            f.each_with_index do|line, line_index|
-                line =line.strip
-                line_number = with_line_number_flag(line, line_index)
-                matches_line_only(line_number, file)
-                matches_entire_line_only(line_number, file)
-            end
-            f.close
-        end
+  def save_matches(line, file)
+    @matched_lines << line
+    @matched_files << file
+  end
+
+  def matches_line_only(line, file)
+    if !matches_entire_line? && line.match?(pattern)
+      save_matches(line, file)
     end
+  end
 
+  def matches_entire_line_only(line, file)
+    if matches_entire_line? && (line.downcase == @keyword.downcase) && line.match?(pattern)
+      save_matches(line, file)
+    end
+  end
+
+  def search_matches
+    @file.map do |file|
+      f = File.open(file, "r")
+      f.each_with_index do |line, line_index|
+        line = line.strip
+        line_number = with_line_number_flag(line, line_index)
+        matches_line_only(line_number, file)
+        matches_entire_line_only(line_number, file)
+      end
+      f.close
+    end
+  end
 end
 
 #feedback
